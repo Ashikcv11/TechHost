bec75c8c0d31709d8b724be7d625fac9210bfb6f
Ashik cv <ashikcv17@gmail.com>
2022-08-07 17:31:55 +0530

    post model connection code changed

--- src/model/PostModel.js
+++ src/model/PostModel.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, (err) => {
-    if (!err){
-        console.log("connected to DB");
-    }
-    else {
-        console.log("Error in connection to DB:" + JSON.stringify(err, undefined, 2));
-    }
+// mongoose.connect(process.env.MONGODB_URI, (err) => {
+//     if (!err){
+//         console.log("connected to DB");
+//     }
+//     else {
+//         console.log("Error in connection to DB:" + JSON.stringify(err, undefined, 2));
+//     }
+// });
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true }).then(()=>{
+    console.log("Database Connection Successful...")
+}).catch((err)=>{
+ console.log(err)
 });
 
 const postSchema = new mongoose.Schema({
