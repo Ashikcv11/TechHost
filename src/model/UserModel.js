2522e0f2a5428357beb8a6d579f741d94ab9909e
Ashik cv <ashikcv17@gmail.com>
2022-08-07 17:32:37 +0530

    user model connection code changed

--- src/model/UserModel.js
+++ src/model/UserModel.js
@@ -1,13 +1,19 @@
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, (err) => {
-    if (!err){
-        console.log("connected to DB");
-    }
-    else {
-        console.log("Error in connection to DB:" + JSON.stringify(err, undefined, 2));
-    }
+// mongoose.connect(process.env.MONGODB_URI, (err) => {
+//     if (!err){
+//         console.log("connected to DB");
+//     }
+//     else {
+//         console.log("Error in connection to DB:" + JSON.stringify(err, undefined, 2));
+//     }
+// });
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true }).then(()=>{
+    console.log("Database Connection Successful...")
+}).catch((err)=>{
+ console.log(err)
 });
 
 const userSchema = new mongoose.Schema({
